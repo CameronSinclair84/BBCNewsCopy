49eb9ea7e5105209b0fce995ae14a18544efb67a
Cameron Sinclair <cameron.sinclair@Nology.local>
2019-04-02 12:43:48 +0100

    Changes

--- build/js/index.js
+++ build/js/index.js
@@ -1,4 +1,20 @@
-var displayMenuItems = function (e) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var newsData_1 = require("./newsData");
+var newsData = newsData_1.default;
+var addToPage = function (article) {
+    var html = "<article>\n    <div class=\"image\" style=\"background-image:url('" + checkImage(article.urlToImage) + "')\"></div>\n    <div class=\"content\">\n    <header>" + article.title + "</header>\n    <small>\n    <span class=\"time\">" + getFriendlyTime(article.publishedAt) + "</span>\n    <span class=\"source\">" + article.source.name + "</span>\n    </small>\n    <div class=\"description\">\n    " + article.description + "\n    </div>\n    <a href=\"" + article.url + "\" target=\"_blank\">Read more</a>\n    </div>\n    </article>";
+    document.getElementById("articles").innerHTML += html;
+};
+var getFriendlyTime = function (date) {
+    var dateObject = new Date(date);
+    return dateObject.toLocaleTimeString();
+};
+var checkImage = function (url) {
+    return (url) ? url : "../assets/placeholder-image.jpg";
+};
+newsData.articles.forEach(addToPage);
+window.displayMenuItems = function (e) {
     e.preventDefault();
     document.getElementById("menulinks").classList.toggle("hidden");
 };
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,57 @@
-const displayMenuItems = (e:MouseEvent) => {
+import data from "./newsData";
+
+interface INewsData {
+    status: String;
+    totalResults: number;
+    articles: IArticleData[];
+}
+
+interface IArticleData {
+    source: {
+        id: string;
+        name: string;
+    },
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+const newsData: INewsData = data;
+
+const addToPage = (article:IArticleData) => {
+    let html = `<article>
+    <div class="image" style="background-image:url('${checkImage(article.urlToImage)}')"></div>
+    <div class="content">
+    <header>${article.title}</header>
+    <small>
+    <span class="time">${getFriendlyTime(article.publishedAt)}</span>
+    <span class="source">${article.source.name}</span>
+    </small>
+    <div class="description">
+    ${article.description}
+    </div>
+    <a href="${article.url}" target="_blank">Read more</a>
+    </div>
+    </article>`;
+    document.getElementById("articles").innerHTML += html;
+}
+
+const getFriendlyTime = (date:string): string => {
+    let dateObject = new Date(date);
+    return dateObject.toLocaleTimeString();
+}
+
+const checkImage = (url:string): string => {
+    return (url) ? url : "../assets/placeholder-image.jpg";
+}
+
+newsData.articles.forEach(addToPage);
+
+(<any>window).displayMenuItems = (e:MouseEvent) => {
     e.preventDefault();
     document.getElementById("menulinks").classList.toggle("hidden");
 }
